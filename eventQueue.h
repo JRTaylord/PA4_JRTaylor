3e144f42dda56890b8797e2de31223034b1aff0a
James Taylor <james.taylor2@comcast.net>
2018-02-12 22:17:08 -0500

    Working on proper implementation of linked list

--- eventQueue.h
+++ eventQueue.h
@@ -4,14 +4,33 @@
 
 #include <iostream>
 #include <queue>
+#include "event.h"
 
 #ifndef PA4_JRTAYLOR_EVENTQUEUE_H
 #define PA4_JRTAYLOR_EVENTQUEUE_H
 
 using namespace std;
 
-class eventQueue: public priority_queue {
+// got code from tutorial at:
+// https://www.codementor.io/codementorteam/a-comprehensive-guide-to-implementation-of-singly-linked-list-using-c_plus_plus-ondlm5azr
+struct node {
+    event data;
+    node *next;
+};
 
+class eventQueue {
+    private:
+        node *head, *tail;
+    public:
+        eventQueue();
+        void createNode();
+        void display();
+        void insert_start(event value);
+        void insert_end(event value);
+        void insert_position(int pos, event value);
+        void delete_first();
+        void delete_last();
+        void delete_position(int pos);
 };
 
 
