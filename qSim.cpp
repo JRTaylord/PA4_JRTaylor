b0293d5781865b098441c03c53245fb2a76a3894
James Taylor <james.taylor2@comcast.net>
2018-02-13 19:15:31 -0500

    Added Tutlis' cmd line argument code

--- qSim.cpp
+++ qSim.cpp
@@ -2,25 +2,47 @@
 // Created by student on 2/10/18.
 //
 #include <iostream>
+#include <cstdlib>
 #include <queue>
 #include "event.h"
 
 using namespace std;
 
-int printQueue(priority_queue<event> q){
-    while (!q.empty()){
-        printf("%d\n",q.top().time);
-        q.pop();
-    }
-    return 0;
-}
 
-int main(){
-    priority_queue<event> events;
-    events.push(new event(1));
-    events.push(1);
-    events.push(new event(3));
-    events.push(new event(2));
-    printQueue(events);
+int main(int argc, char *argv[]){
+    srand(time(0));
+
+    //interpreting command line arguments
+    // total number of customers and tellers
+    int customers, tellers;
+    // time in unit of minutes
+    float simulationTime, averageServiceTime;
+    // random seed for program
+    unsigned int seed;
+
+    if (argc != 5 && argc != 6) {
+        std::cout << "The command line arguments are invalid!\n";
+        std::cout
+                << "./qSim #customers #tellers simulationTime averageServiceTime <seed>\n";
+        return EXIT_FAILURE;
+    }
+
+    // Initializes command line arguments
+    customers = atoi(argv[1]);
+    tellers = atoi(argv[2]);
+    simulationTime = atof(argv[3]);
+    averageServiceTime = atof(argv[4]);
+
+    switch (argc) {
+        case 5:
+            seed = rand();
+            srand(seed);
+            break;
+        case 6:
+            seed = atoi(argv[5]);
+            srand(seed);
+            break;
+    }
+
     return 0;
 }
